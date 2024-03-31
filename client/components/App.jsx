136420d5db03362102a994e417fb233b254750c7
Nicolette Sookar <nicolettesookar@gmail.com>
2024-03-30 22:46:04 -0400

    with directions, better react, minimal styling

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,106 +1,76 @@
 import React, { useState, useEffect } from 'react';
+import { render } from 'react-dom';
+import Map from './Map.jsx'
 
 const App = () => {
-const [locations, setLocations] = useState([])
-const [lat, setLat] = useState()
-const [lon, setLon] = useState()
-const [directionsService, setdirectionsService] = useState()
-const [directionsRenderer, setdirectionsRenderer] = useState()
-let dataArray = []
-// let directionsService;
-// let directionsRenderer;
-//let lat;
-//let lon;
+  const [locations, setLocations] = useState([])
+  const [lat, setLat] = useState()
+  const [lon, setLon] = useState()
+  const [currentFountain, setCurrentFountain] = useState()
+  let dataArray = []
+  // let directionsService;
+  // let directionsRenderer;
+  //let lat;
+  //let lon;
 
-const handleClick = (e, fountainLatitude, fountainLongitude) => {
-  console.log('i am clicking')
-  calculateAndDisplayRoute(directionsService, directionsRenderer, fountainLatitude, fountainLongitude, lat, lon)
-}
-
-const initMap =  (latitude, longitude, data) => {
-//console.log('latitude is', latitude)
-  // directionsService = new google.maps.DirectionsService();
-  // directionsRenderer = new google.maps.DirectionsRenderer();
-  const dirRender = new google.maps.DirectionsRenderer()
-  setdirectionsService(new google.maps.DirectionsService())
-  setdirectionsRenderer(dirRender)
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 17,
-    center: { lat: latitude, lng: longitude },
-  });
-  console.log(directionsService, directionsRenderer)
-  //directionsRenderer.setMap(map);
-  dirRender.setMap(map)
-}
-
-
-function calculateAndDisplayRoute(directionsService, directionsRenderer, fountainLatitude, fountainLongitude, lat, lon) {
-  console.log(lat, lon, fountainLatitude, fountainLongitude)
-  directionsService
-    .route({
-      origin: {
-        query: `${lat},${lon}`,
-      },
-      destination: {
-        query: `${fountainLongitude},${fountainLatitude}`,
-      },
-      travelMode: google.maps.TravelMode.WALKING,
-    })
-    .then((response) => {
-      directionsRenderer.setDirections(response);
-    })
-    .then(response => console.log(response))
-    .catch((e) => window.alert("Directions request failed due to " + e));
-}	
-
-
-const getFountains = (lat, lon) => {
-  const body = {}
-  console.log('in fetch request', lat, lon)
-  body.position = [lat, lon]
-  //console.log(body.position)
-  fetch('/getWaterFountains', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'Application/JSON'
-      },
-      body: JSON.stringify(body)
-    })
-    .then(resp => resp.json())
-    .then((data) => {
-      setLocations(data)
-      console.log(data)
-    })
+  const handleClick = (e, fountainLongitude, fountainLatitude) => {
+    console.log('i am clicking')
+    console.log('fountain coords', fountainLatitude, fountainLongitude)
+    setCurrentFountain({"latitude": fountainLatitude, "longitude":fountainLongitude})
   }
 
-  const getLocation = async (e) => {
-      //console.log('i am getting location')
-       navigator.geolocation.getCurrentPosition((position) => {
-          setLat(position.coords.latitude)
-          setLon(position.coords.longitude)
-          initMap(position.coords.latitude, position.coords.longitude);
-          getFountains(position.coords.latitude, position.coords.longitude)
+
+  const getFountains = (lat, lon) => {
+    const body = {}
+    console.log('in fetch request', lat, lon)
+    body.position = [lat, lon]
+    //console.log(body.position)
+    fetch('/getWaterFountains', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'Application/JSON'
+        },
+        body: JSON.stringify(body)
       })
-  }
+      .then(resp => resp.json())
+      .then((data) => {
+        setLocations(data)
+        console.log(data)
+      })
+    }
+
+    const getLocation = async (e) => {
+        //console.log('i am getting location')
+        navigator.geolocation.getCurrentPosition((position) => {
+            setLat(position.coords.latitude)
+            setLon(position.coords.longitude)
+            getFountains(position.coords.latitude, position.coords.longitude)
+        })
+    }
 
     for(let location of locations){
-      dataArray.push(<button className='waterFountainLocation' onClick={(e => handleClick(e, location.x,location.y))}>
-        <p>{location.location}</p>  
-        <p>{location.description}</p>  
-        <p>{location.distance}</p></button>)
+      dataArray.push(<button className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+      onClick={e => handleClick(e, location.x, location.y)}>
+        <p>Location: {location.location}</p>  
+        {location.description && <p>Details: {location.description}</p>} 
+        <p>{Math.round(location.distance,3)} Miles</p></button>)
       //dataArray.push(<button>{location.x} {location.y}</button>)
     }
 
+    console.log('i am rerendering')
     return (
-    <>
-    <button onClick={(e => getLocation(e))}>Get location</button> 
-     {dataArray}
-    </>
+      <div className="container py-10 px-10 mx-0 min-w-full flex flex-col items-center">
+      <button 
+      type = "button"
+      className="mx-120 text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+      onClick={(e => getLocation(e))}>Get location
+      </button> 
+        {dataArray}
+        <Map lat={lat} lon={lon} currentFountain={currentFountain}></Map>
+      </div>
     )
+  
 }
 
-const Data = (props) => {
-  //console.log(props)
-}
 
 export default App
---
+++ client/components/Map.jsx
@@ -0,0 +1,81 @@
+// const Map = function(props){
+//     return <div id="map" className="mx-96 my-41 object-scale-down w-6/12 bg-red-50"></div>
+//   }
+/*global google*/
+import React, { useState, useEffect } from 'react'
+import { GoogleMap, useLoadScript, Marker, DirectionsRenderer} from '@react-google-maps/api';
+
+const libraries = ['places'];
+const mapContainerStyle = {
+  width: '100vw',
+  height: '100vh',
+};
+
+const Map = (props) => {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: 'AIzaSyCzWRVuy_IO3RyiAqvb5P3YlIT8Xp3jdew',
+    libraries,
+  });
+  const [directions, setDirections] = useState()
+
+  const directionsService = new window.google.maps.DirectionsService();
+
+  useEffect(() => {
+    // Update the document title using the browser API
+    if(props.currentFountain){
+        directionsService.route(
+            {
+                origin: `${props.lat},${props.lon}`,
+                destination: `${props.currentFountain.latitude},${props.currentFountain.longitude}`,
+                travelMode: window.google.maps.TravelMode.WALKING
+            },
+            (result, status) => {
+                if (status === window.google.maps.DirectionsStatus.OK) {
+                    console.log('status ok')
+                    setDirections(result)
+                } else {
+                    console.log(result)
+                    console.error(`error fetching directions ${result}`);
+                }
+            }
+        );
+      }
+  }, [props.currentFountain]);
+
+
+  const center = {
+    lat: props.lat, // default latitude
+    lng: props.lon, // default longitude
+  };
+
+  if (loadError) {
+    return <div>Error loading maps</div>;
+  }
+
+  if (!isLoaded) {
+    return <div>Loading maps</div>;
+  }
+
+ 
+console.log('directions', directions)
+  if(!props.lat) return (
+    <div id ='map'></div>
+  )
+  
+  return (
+    <div id = 'map'>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={17}
+        center={center}
+      >
+        <Marker position={center} />
+        {directions && <DirectionsRenderer directions={directions} />}
+      </GoogleMap>
+    </div>
+  );
+};
+
+
+
+export default Map;
